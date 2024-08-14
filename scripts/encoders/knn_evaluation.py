f0fd5f8a473ac912c64882f7f6e5d1e2d7430530
PauloGamarra <paulogamarralessa@gmail.com>
2024-08-14 19:12:14 -0300

    resnet encodings are done and started knn_evaluation

---
+++ scripts/encoders/knn_evaluation.py
@@ -0,0 +1,12 @@
+import os
+import sklearn
+
+encodings_dir = sys.argv[1]
+
+pre_processings = ['crop_original', 'crop_segmented']
+origins = ['desaparecido', 'procurase_dono']
+speciess = ['cao', 'gato']
+
+
+
+
